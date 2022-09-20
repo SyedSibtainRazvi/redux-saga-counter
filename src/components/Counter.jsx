f9765223e555a8956cbfb0495ada0d0dd94e9c74
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-09-20 17:48:11 +0530

    feat: increment on even

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { DECREMENT, INCREMENT, INCREMENT_ASYNC } from "../features/reducers";
+import { DECREMENT, INCREMENT, INCREMENT_ASYNC, EVEN_INCREMENT } from "../features/reducers";
 
-const CounterDumb = ({ value, onIncrementAsync, onIncrement, onDecrement }) => (
+const CounterDumb = ({ value, onIncrementAsync, onIncrement, onDecrement, onEvenIncrement }) => (
   <div>
     <button onClick={onIncrement}>+</button>{" "}
     <button onClick={onDecrement}>-</button>
     <button onClick={onIncrementAsync}>Increment Async</button>{" "}
+    <button onClick={onEvenIncrement}> Increment on Even</button>
     <hr />
     <h1>{value}</h1>
   </div>
@@ -18,4 +19,5 @@ export const Counter = connect(state => ({ value: state }), {
   onIncrement: action(INCREMENT),
   onDecrement: action(DECREMENT),
   onIncrementAsync: action(INCREMENT_ASYNC),
+  onEvenIncrement: action(EVEN_INCREMENT),
 })(CounterDumb);
--- src/features/reducers.js
+++ src/features/reducers.js
@@ -1,6 +1,7 @@
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 export const INCREMENT_ASYNC = "INCREMENT_ASYNC";
+export const EVEN_INCREMENT = "EVEN_INCREMENT"
 
 export function reducer(state = 0, action) {
     switch (action.type) {
@@ -8,6 +9,11 @@ export function reducer(state = 0, action) {
             return state + 1;
         case DECREMENT:
             return state - 1;
+        case EVEN_INCREMENT:
+            if (state % 2 === 0) {
+                return state + 1
+            } else
+                return state
         default:
             return state;
     }
